1ae62920834c04c9c05b47c6695da2aeeda345fd
Yasser Ghamlouch <yasser@Yassers-MacBook-Pro.local>
2020-05-03 18:09:09 -0500

    add predictive search based on city name

--- src/components/explore/search/search.jsx
+++ src/components/explore/search/search.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import GymInfo from "../gymInfo";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 
 import "./search.scss";
 
@@ -10,9 +14,58 @@ function Search() {
   const [query, setQuery] = useState("");
   const [result, setResult] = useState([]);
 
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 500,
+  });
+
   const handleInputChange = (event) => {
-    setQuery(event.target.value);
+    setValue(event.target.value);
   };
+
+  const handleSelect = ({
+    description,
+    structured_formatting: { main_text },
+  }) => () => {
+    // When user selects a place, we can replace the keyword without request data from API
+    // by setting the second parameter as "false"
+    setValue(description, false);
+    clearSuggestions();
+
+    setQuery(main_text);
+
+    // Get latitude and longitude via utility functions
+    getGeocode({ address: description })
+      .then((results) => getLatLng(results[0]))
+      .then(({ lat, lng }) => {
+        console.log("📍 Coordinates: ", { lat, lng });
+      })
+      .catch((error) => {
+        console.log("😱 Error: ", error);
+      });
+  };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
 
   useEffect(() => {
     if (query === "") {
@@ -29,11 +82,20 @@ function Search() {
   return (
     <div className="search-container container d-flex flex-column">
       <div className="search-bar d-flex justify-content-center">
-        <input
-          placeholder="Enter city name"
-          type="text"
-          onChange={handleInputChange}
-        />
+        <div className="search-bar d-flex flex-column">
+          <input
+            type="text"
+            value={value}
+            onChange={handleInputChange}
+            disabled={!ready}
+            placeholder="Enter a city name"
+          />
+          <div>
+            {status === "OK" && (
+              <ul className="suggestion-list">{renderSuggestions()}</ul>
+            )}
+          </div>
+        </div>
       </div>
       <div className="search-result-container d-flex justify-content-center">
         {result.map((gym) => (
